1ff501a5bc177e3c994c28e65edcc74d9d21b04a
julia <julia.pelegrini26@gmail.com>
2020-09-21 22:41:42 -0300

    componente controlado

---
+++ src/data/Produtos.js
@@ -0,0 +1,12 @@
+export default [
+    { id: 1, nome: "lapis", preco: 0.5 },
+    { id: 2, nome: "caneta", preco: 1.99 },
+    { id: 3, nome: "caderno", preco: 12.99 },
+    { id: 4, nome: "borracha", preco: 2.0 },
+    { id: 5, nome: "lapiseira", preco: 5.89 },
+    { id: 6, nome: "mochila", preco: 90.0 },
+    { id: 7, nome: "apontador", preco: 2.5 },
+    { id: 8, nome: "planner", preco: 19.99 },
+    { id: 9, nome: "agenda", preco: 19.99 },
+  ];
+  
--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import ReactDOM from "react-dom";
+import React from "react";
+import "./App.css";
 
+import Aleatorio from "./componentes_basicos/basico/Aleatorio";
+import Primeiro from "./componentes_basicos/basico/Primeiro";
+import ComParametro from "./componentes_basicos/basico/ComParametro";
+import Card from "./componentes_basicos/layout/Card";
+import Familia from "./componentes_basicos/basico/Familia";
+import FamiliaMembro from "./componentes_basicos/basico/FamiliaMembro";
+import ListaAlunos from "./componentes_basicos/repeticao/ListaAlunos";
+import TabelaProdutos from "./componentes_basicos/repeticao/TabelaProdutos";
+import ParOuImpar from "./componentes_basicos/condicional/ParOuImpar";
+import UsuarioInfo from "./componentes_basicos/condicional/Usuarioinfo";
+import DiretaPai from "./componentes_basicos/comunicacao/DiretaPai"
+import IndiretaPai from "./componentes_basicos/comunicacao/IndiretaPai"
+import Input from './componentes_basicos/formulario/Input'
+
+const el = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <div className="App">
+    <h1>Fundamentos do React</h1>
+    <div className="Cards">
+      <Card titulo="Primeiro componente" color="#E8B71A">
+        <Primeiro> </Primeiro>
+      </Card>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      <Card titulo="Com parâmetro" color="#E94C6F">
+        <ComParametro titulo="vamos la" dois="foi" />
+      </Card>
+
+      <Card titulo="Desafio Aleatório" color="#FA6900">
+        <Aleatorio min={1} max={10} />
+      </Card>
+
+      <Card titulo="Componente com Filhos" color="#00c8f8">
+        <Familia sobrenome="Machado" />
+        <FamiliaMembro nome="Pedro" />
+        <FamiliaMembro nome="Julia" />
+        <FamiliaMembro nome="Jonathan" />
+      </Card>
+
+      <Card titulo="Repetição" color="#3a9ad9">
+        <ListaAlunos></ListaAlunos>
+      </Card>
+
+      <Card titulo="Produtos" color="#ff4c65">
+        <TabelaProdutos></TabelaProdutos>
+      </Card>
+
+      <Card titulo="Renderização condicional" color="#982395">
+        <ParOuImpar numero={19}></ParOuImpar>
+        <UsuarioInfo usuario={{ nome: "Fernando" }}></UsuarioInfo>
+      </Card>
+
+      <Card titulo="Comunicação Direta" color="#59323C">
+      <DiretaPai></DiretaPai>
+      </Card>
+
+      <Card titulo="Comunicação Indireta" color="#8bad39">
+      <IndiretaPai></IndiretaPai>
+      </Card>
+
+      <Card titulo="Componente Controlado" color="#e45f56">
+      <Input></Input>
+      </Card>
+    </div>
+  </div>,
+  el
+);
